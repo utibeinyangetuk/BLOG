846073e5079166acf3d9684a702c07813c48b4f0
utibe <inyangutibz@gmail.com>
2021-10-04 20:32:12 +0100

    Accountcontroller setup

--- controllers/accountcontroller.js
+++ controllers/accountcontroller.js
@@ -1,13 +1,32 @@
 module.exports = {
-  index: (req, res) => {
-    res.render("account", {
-      title: "Account",
-      user: req.user.username,
-    })
-  },
-  logout: (req, res) => {
-    req.logout()
-    req.flash("success_msg", "You have been logged out successfully")
-    res.redirect("/login")
-  },
+	index: (req, res) => {
+		res.render("account", {
+			title: "Account",
+			user: req.user.username,
+		})
+	},
+
+	blog: (req, res) => {
+		res.render("blog", {
+			title: "Recent posts",
+		})
+	},
+	dashboard: (req, res) => {
+		res.render("dashboard", {
+			title: "dashboard",
+		})
+	},
+
+	createpost: (req, res) => {
+		res.render("createpost")
+	},
+	managepost: (req, res) => {
+		res.render("managepost")
+	},
+
+	logout: (req, res) => {
+		req.logout()
+		req.flash("success_msg", "You have been logged out successfully")
+		res.redirect("/login")
+	},
 }
--- routes/account.js
+++ routes/account.js
@@ -1,15 +1,25 @@
 const router = require("express").Router()
-const { index, logout } = require("../controllers/accountcontroller")
+const {
+	index,
+	blog,
+	dashboard,
+	createpost,
+	managepost,
+	logout,
+} = require("../controllers/accountcontroller")
 
 const Notauthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect("/login")
+	if (req.isAuthenticated()) {
+		return next()
+	}
+	res.redirect("/login")
 }
 
 router.get("/", Notauthenticated, index)
-
+router.get("/blog", blog)
+router.get("/dashboard", dashboard)
+router.get("/createpost", createpost)
+router.get("/managepost", managepost)
 router.get("/logout", logout)
 
 module.exports = router
