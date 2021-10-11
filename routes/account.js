6863f793406a994c8ce5d610ba94557fb5411f4e
utibe <inyangutibz@gmail.com>
2021-10-11 20:05:46 +0100

    Added update feature

--- routes/account.js
+++ routes/account.js
@@ -19,13 +19,46 @@ router.get("/dashboard", Notauthenticated, dashboard);
 router.get("/createposts", Notauthenticated, createposts);
 router.get("/manageposts", Notauthenticated, manageposts);
 router.get("/posts/:id", readposts);
-router.get("/posts/delete/:id", deleteposts);
 
+
+
+router.get("/posts/update/:id", async (req, res) => {
+	let { id } = req.params;
+	let post = [];
+	if (id) {
+		post = await knex("posts").where("id", id).first();
+	}
+	return res.render("updateposts", {
+		post: post,
+		title: "update your post",
+	});
+});
+router.get("/posts/delete/:id", deleteposts);
 
 router.get("/logout", Notauthenticated, logout);
 
 //Post routes
 router.post("/", Insertpost);
 router.post("/comments/:post_id", Comments);
+
+router.post("/posts/update/:id", async (req, res) => {
+	let { id } = req.params;
+	let { title, content } = req.body;
+	if (id) {
+		await knex("posts")
+			.update({
+				title: title,
+				content: content,
+			})
+			.where("id", id)
+			.then(() => {
+				req.flash("success_msg", "Your post has been updated✅");
+				res.redirect("/account/manageposts");
+			})
+			.catch((err) => {
+				throw err;
+			});
+	}
+});
 
 module.exports = router
